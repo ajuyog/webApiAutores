704763a545855082cde0bde8267c92dac67a56e9
ajuyog <ajuyog77@gmail.com>
2022-09-01 06:34:13 -0500

    Agregar archivos de proyecto.

---
+++ webApiAutores/Entidades/Autor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webApiAutores.Entidades
+{
+    public class Autor
+    {
+        public static Task<ActionResult<List<Autor>>> AnyAsync { get; internal set; }
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public List<Libro> Libros { get; set; }
+    }
+}
---
+++ webApiAutores/Migrations/20220830032649_ReInicial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webApiAutores.Migrations
+{
+    public partial class ReInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
